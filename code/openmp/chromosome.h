05beecd254edd5c682db4a29e54cd662b9eefb55
hxzhu <hxzhu@umich.edu>
2022-12-17 20:31:06 -0500

    add some tests

---
+++ code/openmp/chromosome.h
@@ -0,0 +1,217 @@
+#include <string>
+#include <bitset>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string.h>
+#include <stdlib.h>
+#include <vector>
+#include <random>
+#include <omp.h>
+using namespace std;
+
+int rand_num_from_range(int start, int end) {
+    return start + rand() % (end - start);
+}
+
+bool repeat(vector<int> s, int c) {
+    for (auto sc : s) {
+        if (sc == c) return true;
+    }
+    return false;
+}
+
+class Chromosome {
+    private:
+        string gnome;
+        vector<int> gnome_vec;
+    public:
+        int fitness;
+        int size; // number of city
+        Chromosome(int size);
+        Chromosome(const Chromosome &t);
+        void create();
+        void reset();
+        // void cross_over(Chromosome& x, Chromosome& y, int p);
+        void cross_over(Chromosome& x, Chromosome& y, int p, vector<vector<int>>& map);
+        vector<int> mutate();
+        string get_gnome();
+        vector<int> get_gnome_vec();
+        void mutate_by_invert();
+};
+
+bool worsethan(Chromosome c1, Chromosome c2) {
+    return c1.fitness < c2.fitness;
+}
+
+Chromosome::Chromosome(int size) {
+    this->size = size;
+    // cout << "chromosome::constructor is called" << endl;
+}
+
+Chromosome::Chromosome(const Chromosome &t) {
+    gnome = t.gnome;
+    gnome_vec = t.gnome_vec;
+    size = t.size;
+    fitness = t.fitness;
+}
+
+string Chromosome::get_gnome() {
+    string gnome_str = "";
+    for (int i = 0; i < gnome_vec.size(); ++i) {
+        gnome_str += to_string(gnome_vec[i]);
+        gnome_str += "->";
+        // cout << "gnome_str: " << gnome_str << endl;
+    }
+    return gnome_str;
+}
+
+vector<int> Chromosome::get_gnome_vec() {
+    return gnome_vec;
+}
+
+void Chromosome::create() {
+    vector<int> g_vec;
+    g_vec.push_back(0);
+    while (true) {
+        if (g_vec.size() == size) {
+            g_vec.push_back(g_vec[0]);
+            break;
+        }
+        int tmp = rand_num_from_range(1, size);
+        if (!repeat(g_vec, tmp)) {
+            g_vec.push_back(tmp);
+        }
+    }
+    // for (auto i: g_vec)
+    //     std::cout << i << ' ';
+    // cout << endl;
+    gnome_vec = g_vec;
+}
+
+int get_index(vector<int> v, int val) {
+    auto it = find(v.begin(), v.end(), val);
+    if (it == v.end()) {
+        return -1;
+    } else {
+        return int(it - v.begin());
+    }
+}
+
+void Chromosome::cross_over(Chromosome& x, Chromosome& y, int p, vector<vector<int>>& map) {
+    vector<int> parent1_gnome_vec = x.get_gnome_vec();
+    vector<int> parent2_gnome_vec = y.get_gnome_vec();
+    vector<int> new_chro;
+    new_chro.resize(size+1, 0);
+    for (int i = 1; i < size; ++i) {
+        int gnome_from_p1, gnome_from_p2;
+        int p1_cand_idx = get_index(parent1_gnome_vec, new_chro[i-1])+1;
+        int p1_cand = parent1_gnome_vec[p1_cand_idx];
+        if (p1_cand == 0 || get_index(new_chro, p1_cand) != -1) {
+            for (int j = 1; j < size; ++j) {
+                if (get_index(new_chro, j) == -1) {
+                    p1_cand = j;
+                    break;
+                }
+            }
+        }
+        // p2
+        int p2_cand_idx = get_index(parent2_gnome_vec, new_chro[i-1])+1;
+        int p2_cand = parent2_gnome_vec[p2_cand_idx];
+        if (p2_cand == 0 || get_index(new_chro, p2_cand) != -1) {
+            for (int j = 1; j < size; ++j) {
+                if (get_index(new_chro, j) == -1) {
+                    p2_cand = j;
+                    break;
+                }
+            }
+        }
+        // compete
+        int p1_fitness = map[new_chro[i-1]][p1_cand];
+        int p2_fitness = map[new_chro[i-1]][p2_cand];
+        new_chro[i] = p1_fitness <= p2_fitness ? p1_cand : p2_cand;
+    }
+    gnome_vec = new_chro;
+}
+
+// void Chromosome::cross_over(Chromosome& x, Chromosome& y, int p) {
+//     vector<int> parent1_gnome_vec = x.get_gnome_vec();
+//     vector<int> parent2_gnome_vec = y.get_gnome_vec();
+//     vector<int> new_chro;
+//     new_chro.resize(x.size+1, 0);
+    
+//     // cout <<"p1: ";
+//     // for(auto x: parent1_gnome_vec) {
+//     //     cout << x << " ";
+//     // }
+//     // cout << endl;
+
+//     // cout <<"p2: ";
+//     // for(auto x: parent2_gnome_vec) {
+//     //     cout << x << " ";
+//     // }
+//     // cout << endl;
+
+//     int g_idx = 1;
+//     for (int i = 1; i < p; i++) {
+//         new_chro[g_idx] = parent1_gnome_vec[i];
+//         g_idx++;
+//     }
+
+//     // cout << "[CV] pivot " << p << endl;
+//     // for(auto x : new_chro) {
+//     //     cout << x << " ";
+//     // }
+//     // cout << endl;
+
+//     int i = 1;
+//     while (g_idx < x.size) {
+//         if (find(new_chro.begin(), new_chro.end(), parent2_gnome_vec[i]) == new_chro.end()) {
+//             // cout << parent2_gnome_vec[i] << endl;
+//             new_chro[g_idx++] = parent2_gnome_vec[i++];
+//         } else {
+//             i++;
+//         }
+//     }
+//     new_chro[g_idx] = 0;
+//     gnome_vec = new_chro;
+//     // cout << "CROSSOVER ONE DONE" <<endl;
+// }
+
+vector<int> Chromosome::mutate() {
+    while (true) {
+        int victim_gene1 = rand_num_from_range(1, size);
+        int victim_gene2 = rand_num_from_range(1, size);
+        if (victim_gene1 != victim_gene2) {
+            int tmp = gnome_vec[victim_gene1];
+            gnome_vec[victim_gene1] = gnome_vec[victim_gene2];
+            gnome_vec[victim_gene2] = tmp;
+            break;
+        }
+    }
+    return gnome_vec;
+} 
+
+void Chromosome::mutate_by_invert() {
+    int victim_gene1_idx = rand_num_from_range(1, size);
+    int victim_gene2_idx = rand_num_from_range(1, size);
+    while (victim_gene2_idx == victim_gene1_idx) {
+        victim_gene2_idx = rand_num_from_range(1, size);
+    }
+    // cout << victim_gene1_idx << " " << victim_gene2_idx << endl;
+
+    if (victim_gene1_idx > victim_gene2_idx) { // swap
+        int tmp = victim_gene1_idx;
+        victim_gene1_idx = victim_gene2_idx;
+        victim_gene2_idx = tmp;
+    }
+    while (victim_gene1_idx < victim_gene2_idx) {
+        int tmp = gnome_vec[victim_gene1_idx];
+        gnome_vec[victim_gene1_idx] = gnome_vec[victim_gene2_idx];
+        gnome_vec[victim_gene2_idx] = tmp;
+        victim_gene1_idx++;
+        victim_gene2_idx--;
+    }
+    return;
+}
---
+++ code/openmp/main.cpp
@@ -0,0 +1,284 @@
+// Sequential Version Genetic Algorithm for Travelling Salesman Problem
+// Author: Haoxuan Zhu (haoxuanz@andrew.cmu.edu)
+// Date: Nov 15, 2022
+#include <limits.h>
+#include "chromosome.h"
+#include <vector>
+#include <cmath>
+#include <random>
+
+#include "brute_force.h"
+
+using namespace std;
+
+#define GEN_THRES 100
+#define POPULATION_SIZE 10000
+#define MATING_SIZE  (POPULATION_SIZE / 500)
+#define TOLERANCE 10
+// #define DEBUG
+
+vector<vector<int>> load_tsp_from_file(string filename) {
+    vector<vector<int>> map;
+    ifstream fp(filename);
+    string line;
+    getline(fp, line);
+    size_t dim = (int)atoi(line.c_str());
+    while (getline(fp, line)) {
+        vector<int> curr_line;
+        stringstream sstream(line);
+        string str;
+        for(int i = 0; i < dim; ++i) {
+            getline(sstream, str, ' ');
+            int val = atoi(str.c_str());
+            val = val == -1 ? INT_MAX : val;
+            curr_line.push_back(val);
+        }
+        map.push_back(curr_line);
+    }
+    fp.close();
+    return map;
+}
+
+int cal_fitness(vector<vector<int>>& map, vector<int> gnome)
+{   
+    // cout << "cal_fitness() is called" << endl;
+    // cout << gnome << endl;
+    int f = 0;
+    for (int i = 0; i < gnome.size() - 1; i++) {
+        if (map[gnome[i]][gnome[i + 1]] == INT_MAX) {
+            // cout << "INT_MAX" << endl;
+            return INT_MAX;
+        }
+        f += map[gnome[i]][gnome[i + 1]];
+        // cout << "f: " << f << endl;
+    }
+    return f;
+}
+
+int uniform_rand(int start, int end) {
+    // std::default_random_engine generator;
+    // std::uniform_int_distribution<int> distribution(start,end-1);
+    // return distribution(generator);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(start, end-1);
+    int res = dis(gen);
+    return res;
+}
+
+void cross_over(vector<Chromosome>& mating_pop, Chromosome& chro1, Chromosome& chro2, vector<vector<int>>& map) {
+    int idx1 = uniform_rand(0, mating_pop.size());
+    int idx2 = uniform_rand(0, mating_pop.size());
+    int pos = uniform_rand(1, chro1.size);
+    chro1.cross_over(mating_pop[idx1], mating_pop[idx2], pos, map);
+    chro2.cross_over(mating_pop[idx2], mating_pop[idx1], pos, map);
+}
+
+vector<Chromosome> selection_tournament(vector<Chromosome>& pop, int mating_size) {
+    vector<Chromosome> mating_pop;
+    int tournament_size = 25;
+    for (int i = 0; i < mating_size; ++i) {
+        Chromosome best_chro = pop[0];
+        int best_fitness = INT_MAX;
+
+        for (int j = 0; j < tournament_size; ++j) {
+            // int victim = uniform_rand(0, pop.size());
+            int victim = 0 + rand() % (pop.size());
+            if (pop[victim].fitness < best_fitness) {
+                best_fitness = pop[victim].fitness;
+                best_chro = pop[victim];
+            }
+        }
+        mating_pop.push_back(best_chro);
+    }
+    
+    return mating_pop;
+}
+
+// vector<Chromosome> selection(vector<Chromosome>& pop, int mating_size) {
+//     vector<Chromosome> mating_pop;
+//     int fitness_min = INT_MAX;
+//     int fitness_max = -INT_MAX;
+//     int fitness_sum = 0;
+//     int fitness_avg = 0;
+//     for (int i = 0; i < pop.size(); ++i) {
+//         fitness_min = fitness_min < pop[i].fitness ? fitness_min : pop[i].fitness;
+//         fitness_max = fitness_max > pop[i].fitness ? fitness_max : pop[i].fitness;
+//         fitness_sum += pop[i].fitness;
+//     }
+//     fitness_avg = fitness_sum / pop.size();
+//     int fitness_thres = 0.25*fitness_avg+1;
+
+//     for (int j = 0; j < pop.size(); j++) {
+//         if (fitness_avg > pop[j].fitness) {
+//             mating_pop.push_back(pop[j]);
+//         }
+//         if (mating_pop.size() == mating_size) {
+//             break;
+//         }
+//     }
+
+//     int cj = pop.size()-1;
+//     while (mating_pop.size() < mating_size) {
+//         mating_pop.push_back(pop[cj--]);
+//     }
+    
+//     return mating_pop;
+// }
+
+vector<Chromosome> selection_by_sort(vector<Chromosome>& population) { // Abandoned
+    vector<Chromosome> mating_pop;
+    sort(population.begin(), population.end(), worsethan);
+    for (int i = 0; i < MATING_SIZE; ++i) {
+        mating_pop.push_back(population[i]);
+    }
+    return mating_pop;
+}
+
+void solver(vector<vector<int>> map, int gen_thres, int pop_size) {
+    int gen = 1;
+    int num_node = map.size();
+    vector<Chromosome> population;
+    for (int i = 0; i < pop_size; ++i) {
+        Chromosome chro = Chromosome(num_node);
+        chro.create();
+        chro.fitness = cal_fitness(map, chro.get_gnome_vec());
+        population.push_back(chro);
+    }
+    Chromosome best_seen_chro = population[0];
+    int best_seen_fitness = INT_MAX;
+    int prev_best_fitness = INT_MAX;
+    int tol = 0;
+
+    #ifdef DEBUG
+    cout << "\nInitial population: " << endl << "GNOME\tFITNESS VALUE\n";
+    for (int i = 0; i < pop_size; i++)
+        cout << population[i].get_gnome() << "\t" << population[i].fitness << endl;
+    cout << "\n";
+    #endif
+
+    cout << "Generation " << gen << " \n";
+    while (gen <= gen_thres) {
+        // selection
+        vector<Chromosome> mating_pop;
+        // mating_pop = selection(population, MATING_SIZE);
+        mating_pop = selection_tournament(population, MATING_SIZE);
+
+        cout << "Mating Pop:" <<endl;
+        vector<int> cnter;
+        cnter.resize(num_node, 0);
+        for (auto c: mating_pop) {
+            cnter[c.get_gnome_vec()[1]]++;
+        }
+        for (auto x: cnter) cout << x << " ";
+        cout <<endl;
+
+        // cout << "Mating Pop:" <<endl;
+        // for(auto x:mating_pop) {
+        //     cout << x.get_gnome() << endl;
+        // }
+        // cout << "SELECTION done " << endl;
+
+        // evoluation
+        vector<Chromosome> new_population;
+        for (int i = 0; i < POPULATION_SIZE; i+=2) {
+            // cross-over
+            Chromosome chro1 = Chromosome(num_node);
+            Chromosome chro2 = Chromosome(num_node);
+            chro1.create();
+            chro2.create();
+
+            cross_over(mating_pop, chro1, chro2, map);
+            #ifdef DEBUG
+            cout << chro1.get_gnome() <<endl;
+            cout << chro2.get_gnome() <<endl;
+            cout << "CROSSOVER done " << endl;
+            #endif
+
+            // mutation
+            if (uniform_rand(0, 10) < 2) {
+                // cout << "MUTATION happened" <<endl;
+                chro1.mutate_by_invert();
+            }
+            if (uniform_rand(0, 10) < 2) {
+                // cout << "STRONG MUTATION happened" <<endl;
+                chro2.mutate_by_invert();
+            }
+            #ifdef DEBUG
+            cout << chro1.get_gnome() <<endl;
+            cout << chro2.get_gnome() <<endl;
+            cout << "MUTATION done " << endl;
+            #endif
+
+            new_population.push_back(chro1);
+            new_population.push_back(chro2);
+        }
+
+        // evaluation
+        for (int i = 0; i < new_population.size(); i++) {
+            new_population[i].fitness = cal_fitness(map, new_population[i].get_gnome_vec());
+            if (new_population[i].fitness < best_seen_chro.fitness) {
+                best_seen_chro = new_population[i];
+                best_seen_fitness = new_population[i].fitness;
+            }
+        }
+        // Chromosome mutated_best_seen_chro = best_seen_chro;
+        // mutated_best_seen_chro.mutate_by_invert();
+        // new_population.push_back(mutated_best_seen_chro);
+        population = new_population;
+        
+
+        #ifdef DEBUG
+        cout << "Generation " << gen << " \n";
+        cout << "GNOME\tFITNESS VALUE\n";
+        sort(population.begin(), population.end(), worsethan);
+        for (int i = 0; i < pop_size; i++)
+            cout << population[i].get_gnome() << "\t"
+                << population[i].fitness << endl;
+        #endif
+        // sort(new_population.begin(), new_population.end(), worsethan);
+        cout << "GEN:" << gen << ":best -> " << best_seen_chro.get_gnome() << "\t" << best_seen_chro.fitness << endl;
+        
+        gen++;
+        if (tol >= TOLERANCE) {
+            break;
+        } else {
+            if (prev_best_fitness == best_seen_fitness) {
+                tol++;
+            } else {
+                tol = 0;
+                prev_best_fitness = best_seen_fitness;
+            }
+        }
+    }
+    cout << "Final Generation " << gen << " \n";
+    #ifdef DEBUG
+    cout << "GNOME\tFITNESS VALUE\n";
+    sort(population.begin(), population.end(), worsethan);
+    for (int i = 0; i < pop_size; i++)
+        cout << population[i].get_gnome() << "\t"
+            << population[i].fitness << endl;
+    #endif
+    cout << "Result" << best_seen_chro.get_gnome() << "\t" << best_seen_chro.fitness << endl;
+}
+
+int main() {
+    vector<vector<int>> map = load_tsp_from_file("../testcase/30.txt");
+    #ifdef DEBUG
+    for(int i = 0; i < map.size(); ++i) {
+        for (int j = 0; j < map[i].size(); ++j) {
+            cout << map[i][j] << " ";
+        }
+        cout << endl;
+    }
+    #endif
+    solver(map, GEN_THRES, POPULATION_SIZE);
+
+    // control group
+    // string ans_str = "";
+	// cout << "brute_force result: " << travllingSalesmanProblem(map, 0, ans_str) << " " << ans_str << endl;
+    // cout << "greedy result: " << tsp_greedy(map) << endl; // not correct
+    return 0;
+}
+
+
